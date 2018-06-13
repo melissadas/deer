ee680be5a31fb5f9ad9b4d6d6c26f53454550987
Travis CI <kvndrsslr@gmail.com>
2018-06-13 12:22:06 +0000

    Deploy to GitHub Pages: 27e67d28d66385543ba08d13e3bb1309e19d8278

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"org.aksw.deer"},{"l":"org.aksw.deer.enrichments"},{"l":"org.aksw.deer.io"},{"l":"org.aksw.deer.learning"},{"l":"org.aksw.deer.util"},{"l":"org.aksw.deer.vocabulary"}]
+packageSearchIndex = [{"l":"org.aksw.deer"},{"l":"org.aksw.deer.enrichments"},{"l":"org.aksw.deer.io"},{"l":"org.aksw.deer.learning"},{"l":"org.aksw.deer.logging"},{"l":"org.aksw.deer.server"},{"l":"org.aksw.deer.util"},{"l":"org.aksw.deer.vocabulary"}]
