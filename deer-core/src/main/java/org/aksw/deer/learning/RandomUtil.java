b5886df2025828326574ef8c8aa53f70c3f5fb22
Kevin Dreßler <kvndrsslr@gmail.com>
2019-05-28 12:27:02 +0200

    implemented global seed of good randomness for evolutionary learning

---
+++ deer-core/src/main/java/org/aksw/deer/learning/RandomUtil.java
@@ -0,0 +1,31 @@
+package org.aksw.deer.learning;
+
+import io.jenetics.prngine.MT19937_64Random;
+import io.jenetics.prngine.Random64;
+
+/**
+ *
+ */
+public class RandomUtil {
+
+  private static final Random64 rng;
+
+  static {
+    rng = new MT19937_64Random.ThreadSafe();
+  }
+
+  public static long get(long min, long max) {
+    return rng.nextLong(min, max);
+  }
+
+  public static long get(long max) {
+    return rng.nextLong(max);
+  }
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 1000; i++) {
+      System.out.println(get(9999));
+    }
+  }
+
+}
